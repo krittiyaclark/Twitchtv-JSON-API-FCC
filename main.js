46e8ddf16b1dc35682fbbaed6f612d47bb630f2e
Krittiya Clark <krittiyaclark@gmail.com>
2017-07-20 15:32:57 -0400

    Set up HTML page structure

---
+++ main.js
@@ -0,0 +1,2 @@
+// Client ID=jo5yejqiz1q6lkhyf08iixvd6lcdkh
+// var follower = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
